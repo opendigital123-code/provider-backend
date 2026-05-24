9b7b8a215080ba8547f2c02e9d466fb849ecde58
opendigital123-code <opendigital123@gmail.com>
2026-05-24 19:48:21 -0400

    Create categories.js

---
+++ src/constants/categories.js
@@ -0,0 +1,17 @@
+const PROVIDER_CATEGORIES = [
+  "barber",
+  "makeup",
+  "esthetician",
+  "hair",
+  "massage",
+];
+
+const MIN_PROVIDER_CATEGORIES = 2;
+
+const NEARBY_RADIUS_KM_DEFAULT = 25;
+
+module.exports = {
+  PROVIDER_CATEGORIES,
+  MIN_PROVIDER_CATEGORIES,
+  NEARBY_RADIUS_KM_DEFAULT,
+};
